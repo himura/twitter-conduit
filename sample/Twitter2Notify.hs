4e0a29cbe45fa08af150777bcc6627ca55157693
Takahiro HIMURA <taka@himura.jp>
2011-12-05 01:41:15 +0900

    Added sample/oslist.hs, cleaned up code.

--- sample/Twitter2Notify.hs
+++ sample/Twitter2Notify.hs
@@ -2,21 +2,14 @@
 
 module Main where
 
+import Common
 import Web.Twitter.Enumerator
 
-import Web.Authenticate.OAuth (OAuth(..), Credential(..))
-import qualified Web.Authenticate.OAuth as OA
-import qualified Network.URI as URI
 import Network.HTTP.Enumerator
-import Data.Aeson hiding (Error)
-import Data.Aeson.Types (parseMaybe)
 import Data.Enumerator hiding (map)
 import qualified Data.Enumerator.List as EL
-import qualified Data.Map as M
 import qualified Data.ByteString.Lazy.Char8 as LB
 import qualified Data.ByteString.Char8 as B
-import qualified Data.CaseInsensitive as CI
-import Data.Attoparsec
 import Control.Monad.IO.Class
 import Control.Applicative
 import qualified Data.Text.Encoding as T
@@ -27,92 +20,18 @@ import System.FilePath
 import System.Directory
 import System.Environment
 
-import Data.List
-import Data.Char
-
-tokens :: OAuth
-tokens = OAuth { oauthServerName = "twitter"
-               , oauthRequestUri = "http://twitter.com/oauth/request_token"
-               , oauthAccessTokenUri = "http://twitter.com/oauth/access_token"
-               , oauthAuthorizeUri = "http://twitter.com/oauth/authorize"
-               , oauthConsumerKey = "<consumer key>"
-               , oauthConsumerSecret = "<consumer secret>"
-               , oauthSignatureMethod = OA.HMACSHA1
-               , oauthCallback = Nothing
-               }
-
 main :: IO ()
 main = withCF $ do
-  (listName:_) <- liftIO getArgs
-  curious <- fetchListAll listName
+  (ln:_) <- liftIO getArgs
+  curious <- fetchListAll ln
   let cid = map userId curious
   run_ $ userstream (EL.mapM (\x -> showTL x >> return x) =$ EL.filter (filterUsers cid) =$ iterNotify)
-  where withCF t = credentialFile >>= \f -> withCredentialFile f t
-        fetchListAll str = run_ $ listsMembers (QListName str) $$ EL.consume
+  where fetchListAll str = run_ $ listsMembers (QListName str) $$ EL.consume
         filterUsers ulist (SStatus s) = (\uid -> any (== uid) ulist) . userId . statusUser $ s
         filterUsers _ _ = False
 
-ensureDirectoryExist :: FilePath -> IO FilePath
-ensureDirectoryExist dir = do
-  createDirectoryIfMissing True dir
-  return dir
-
-confdir :: IO FilePath
-confdir = fmap (</> ".twitter2notify") getHomeDirectory >>= ensureDirectoryExist
-
-credentialFile :: IO FilePath
-credentialFile = (</> "credential.json") <$> confdir
-
 iconPath :: IO FilePath
 iconPath = (</> "icons") <$> confdir >>= ensureDirectoryExist
-
-getProxyEnv :: IO (Maybe Proxy)
-getProxyEnv = do
-  env <- M.fromList <$> map (\(k,v) -> (CI.mk k, v)) <$> getEnvironment
-  let u = M.lookup "https_proxy" env <|>
-          M.lookup "http_proxy" env <|>
-          M.lookup "proxy" env >>= URI.parseURI >>= URI.uriAuthority
-  return $ Proxy <$> (B.pack . URI.uriRegName <$> u) <*> (parsePort . URI.uriPort <$> u)
-  where
-    parsePort :: String -> Int
-    parsePort []       = 8080
-    parsePort (':':xs) = read xs
-    parsePort xs       = error $ "port number parse failed " ++ xs
-
-loadCredential :: FilePath -> IO (Maybe Credential)
-loadCredential file = do
-  existp <- doesFileExist file
-  if existp
-    then
-    do
-      content <- B.readFile file
-      return $ (maybeResult . parse json) content >>= parseMaybe parseJSON >>= return . Credential
-    else return Nothing
-
-saveCredential :: FilePath -> Credential -> IO ()
-saveCredential file cred = do
-  LB.writeFile file $ encode . unCredential $ cred
-
-withCredentialFile :: FilePath -> TW a -> IO a
-withCredentialFile file task = do
-  pr <- getProxyEnv
-  cred <- maybe (authorize pr tokens getPIN) return =<< loadCredential file
-  saveCredential file cred
-  let env = newEnv tokens
-  runTW env { twCredential = cred, twProxy = pr } $ task
-  where
-    getPIN url = do
-      putStrLn $ "browse URL: " ++ url
-      putStr "> what was the PIN twitter provided you with? "
-      hFlush stdout
-      getLine
-
-authorize :: Maybe Proxy -> OAuth -> (String -> IO String) -> IO Credential
-authorize pr oauth getPIN = do
-  cred <- OA.getTemporaryCredentialProxy pr oauth
-  let url = OA.authorizeUrl oauth cred
-  pin <- getPIN url
-  OA.getAccessTokenProxy pr oauth $ OA.insert "oauth_verifier" (B.pack pin) cred
 
 fetchIcon :: String -> String -> IO String
 fetchIcon scName url = do
@@ -128,7 +47,7 @@ fetchIcon scName url = do
 notifySend :: String -> String -> Maybe FilePath -> IO ()
 notifySend header content icon = do
   let ic = maybe [] (\i -> ["-i", i]) icon
-  rawSystem "notify-send" $ [header, content] ++ ic
+  _ <- rawSystem "notify-send" $ [header, content] ++ ic
   return ()
 
 iterNotify :: Iteratee StreamingAPI IO ()
