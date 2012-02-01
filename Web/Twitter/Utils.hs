6ee0188b86e77c064bc1c05ede2335f32f4940b2
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-02-01 18:59:11 +0900

    add sinkJSON'

--- Web/Twitter/Utils.hs
+++ Web/Twitter/Utils.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Web.Twitter.Utils
        ( enumJSON
        , sinkJSON
+       , sinkJSON'
        , conduitParser
        , skipNothing
        , debugEE
@@ -8,13 +11,21 @@ module Web.Twitter.Utils
        )
        where
 
+import Control.Exception
 import Control.Monad.Trans
 import Data.Aeson hiding (Error)
-import Data.Aeson.Types (parseMaybe)
+import qualified Data.Aeson.Types as AT
 import qualified Data.Attoparsec.Types as A
 import qualified Data.Conduit as C
 import qualified Data.Conduit.Attoparsec as CA
 import Data.ByteString (ByteString)
+import Data.Data
+
+data TwitterError
+  = TwitterError String
+  deriving (Show, Data, Typeable)
+
+instance Exception TwitterError
  
 skipNothing :: Monad m => C.Conduit (Maybe a) m a
 skipNothing = undefined -- EL.concatMap (\x -> [fromJust x | isJust x])
@@ -23,7 +34,7 @@ debugEE :: (MonadIO m, Show a) => C.Conduit a m a
 debugEE = undefined -- EL.mapM $ \x -> (liftIO . putStrLn . show) x >> return x
 
 fromJSON' :: FromJSON a => Value -> Maybe a
-fromJSON' = parseMaybe parseJSON
+fromJSON' = AT.parseMaybe parseJSON
 
 enumJSON :: Monad m => C.Conduit ByteString m Value
 enumJSON = undefined -- E.sequence $ iterParser json
@@ -31,12 +42,12 @@ enumJSON = undefined -- E.sequence $ iterParser json
 sinkJSON :: C.ResourceThrow m => C.Sink ByteString m Value
 sinkJSON = CA.sinkParser json
 
-sinkJSON' :: (FromJSON a, C.ResourceThrow m) => C.Sink ByteString m a
+sinkJSON' :: (FromJSON a, C.ResourceIO m) => C.Sink ByteString m a
 sinkJSON' = do
   v <- sinkJSON
-  case fromJSON' v of
-    -- Nothing -> C.resourceThrow $ ParseException v []
-    Just r -> return r
+  case fromJSON v of
+    AT.Error err -> lift $ liftIO $ throwIO $ TwitterError err
+    AT.Success r -> return r
 
 conduitParser :: (CA.AttoparsecInput a, C.ResourceThrow m) => A.Parser a b -> C.Conduit a m b
 conduitParser p =
