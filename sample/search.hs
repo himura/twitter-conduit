1bb6374f24d7f7d4c2bd82c412c950733d682b17
Takahiro HIMURA <taka@himura.jp>
2013-12-21 18:38:42 +0900

    fix search.hs

--- sample/search.hs
+++ sample/search.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Monad.Trans
+import Control.Monad.IO.Class
 import System.Environment
 
 import Web.Twitter.Conduit
 import Data.Conduit
 import qualified Data.Conduit.List as CL
 import Data.Default
+import Common
 
 main :: IO ()
-main = runTW (def :: TWInfo NoToken) $ do
+main = withCF $ do
   [keyword] <- liftIO getArgs
 
   res <- searchSource keyword []
-  liftIO . putStrLn $ "search completed in: " ++ (show . searchResultCompletedIn $ res)
-  liftIO . putStrLn $ "search result max id: " ++ (show . searchResultMaxId $ res)
-  searchResultResults res $$ CL.mapM_ (liftIO . print)
+  let metadata = searchResultSearchMetadata res
+  liftIO . putStrLn $ "search completed in: " ++ (show . searchMetadataCompletedIn $ metadata)
+  liftIO . putStrLn $ "search result max id: " ++ (show . searchMetadataMaxId $ metadata)
+  searchResultStatuses res $$ CL.mapM_ (liftIO . print)
