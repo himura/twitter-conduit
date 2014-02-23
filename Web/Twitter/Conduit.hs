6586b90e27c2588baf61262da622ebf2804a9a03
Takahiro HIMURA <taka@himura.jp>
2014-02-23 20:34:46 +0900

    export Request

--- Web/Twitter/Conduit.hs
+++ Web/Twitter/Conduit.hs
@@ -8,4 +8,5 @@ import Web.Twitter.Conduit.Status as Export
 import Web.Twitter.Conduit.Monad as Export
 import Web.Twitter.Conduit.Param as Export
 import Web.Twitter.Conduit.Stream as Export
+import Web.Twitter.Conduit.Request as Export
 import Web.Twitter.Types as Export
