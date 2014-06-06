92d6432144bc4417c9c2e67244a7613ac0f002e0
Takahiro HIMURA <taka@himura.jp>
2014-06-07 03:15:29 +0900

    fix doctest

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -8,21 +8,12 @@ import Control.Monad
 import Control.Applicative
 
 main = do
-  sources <- findSources "Web"
-  doctest $ args ++ sources
+  -- sources <- findSources "Web"
+  doctest $ args -- ++ sources
   where
     args = [ "-i."
            , "-idist/build/autogen"
            , "-optP-include"
            , "-optPdist/build/autogen/cabal_macros.h"
+           , "Web/Twitter/Conduit.hs"
            ]
-
-findSources :: FilePath -> IO [FilePath]
-findSources dir = filter (isSuffixOf ".hs") <$> go dir
-  where
-    go dir = do
-      (dirs, files) <- listFiles dir
-      (files ++) . concat <$> mapM go dirs
-    listFiles dir = do
-      entries <- map (dir </>) . filter (`notElem` [".", ".."]) <$> getDirectoryContents dir
-      (,) <$> filterM doesDirectoryExist entries <*> filterM doesFileExist entries
