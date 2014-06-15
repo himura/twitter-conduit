a334d652befcb0ec69046674c70d0f9ea3ad9c5e
Takahiro HIMURA <taka@himura.jp>
2014-06-15 09:50:13 +0900

    Add more tests

--- tests/StatusSpec.hs
+++ tests/StatusSpec.hs
@@ -2,8 +2,10 @@
 
 module StatusSpec where
 
-import Web.Twitter.Conduit (call, accountVerifyCredentials)
-import Web.Twitter.Conduit.Status
+import Data.Conduit
+import qualified Data.Conduit.List as CL
+import Web.Twitter.Conduit (call, accountVerifyCredentials, sourceWithMaxId)
+import Web.Twitter.Conduit.Status as Status
 import qualified Web.Twitter.Conduit.Parameters as Param
 import Web.Twitter.Types.Lens
 import Control.Lens
@@ -21,15 +23,15 @@ spec = do
     describe "mentionsTimeline" $ do
         it "returns the 20 most resent mentions for user" $ do
             res <- run $ call mentionsTimeline
-            res `shouldSatisfy` ((> 0) . length)
+            length res `shouldSatisfy` (> 0)
             let mentionsScreenName = res ^.. traversed . statusEntities . _Just . enUserMentions . traversed . entityBody . userEntityUserScreenName
             mentionsScreenName `shouldSatisfy` allOf folded (== (self ^. userScreenName))
-            mentionsScreenName `shouldSatisfy` ((== length res) . length)
+            length mentionsScreenName `shouldSatisfy` (== length res)
 
     describe "userTimeline" $ do
         it "returns the 20 most recent tweets posted by the user indicated by ScreenNameParam" $ do
             res <- run . call $ userTimeline (Param.ScreenNameParam "thimura")
-            res `shouldSatisfy` ((== 20) . length)
+            length res `shouldSatisfy` (== 20)
             res `shouldSatisfy` (allOf folded (^. statusUser . userScreenName . to (== "thimura")))
         it "returns the recent tweets which include RTs when specified include_rts option" $ do
             res <- run . call
@@ -40,13 +42,45 @@ spec = do
     describe "homeTimeline" $ do
         it "returns the most recent tweets in home timeline" $ do
             res <- run $ call homeTimeline
-            res `shouldSatisfy` ((> 0) . length)
+            length res `shouldSatisfy` (> 0)
+
+        it "iterate with sourceWithMaxId" $ do
+            tl <- run $ do
+                let src = sourceWithMaxId $ homeTimeline & Param.count ?~ 200
+                src $$ CL.isolate 600 =$ CL.consume
+            length tl `shouldSatisfy` (== 600)
+
+            let ids = tl ^.. traversed . statusId
+            zip ids (tail ids) `shouldSatisfy` all (\(a, b) -> a > b)
 
     describe "retweetsOfMe" $ do
         let response = unsafePerformIO . run . call $ retweetsOfMe
         it "returns the most recent tweets authored by the authenticating user" $ do
              let screenNames = response ^.. traversed . statusUser . userScreenName
              screenNames `shouldSatisfy` allOf folded (== (self ^. userScreenName))
-             screenNames `shouldSatisfy` ((== length response) . length)
+             length screenNames `shouldSatisfy` (== length response)
         it "returns the most recent tweets that have been retweeted by other" $ do
             response `shouldSatisfy` allOf (traversed . statusRetweetCount . _Just) (> 0)
+
+    describe "showId" $ do
+        it "works for the known tweets" $ do
+            res <- run . call $ showId 477833886768959488
+            res ^. statusId `shouldBe` 477833886768959488
+            res ^. statusText `shouldBe` "真紅かわいいはアレセイア"
+            res ^. statusCreatedAt `shouldBe` "Sat Jun 14 15:24:10 +0000 2014"
+            res ^. statusUser . userScreenName `shouldBe` "thimura"
+
+    describe "update & destroyId" $ do
+        it "posts new tweet and destroy it" $ do
+            res1 <- run . call $ update "おまえの明日が、今日よりもずっと、楽しい事で溢れているようにと、祈っているよ"
+            res1 ^. statusUser . userScreenName `shouldBe` self ^. userScreenName
+
+            res2 <- run . call $ destroyId (res1 ^. statusId)
+            res2 ^. statusId `shouldBe` res1 ^. statusId
+
+    describe "lookup" $ do
+        it "works for the known tweets" $ do
+            res <- run . call $ Status.lookup [438691466345340928, 477757405942411265]
+            length res `shouldSatisfy` (== 2)
+            (res !! 0) ^. statusId `shouldBe` 438691466345340928
+            (res !! 1) ^. statusId `shouldBe` 477757405942411265
