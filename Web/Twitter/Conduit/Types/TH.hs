8a67eaed8a4e4c437aaf59e8ea819c9af6052244
Takahiro HIMURA <taka@himura.jp>
2014-08-23 09:16:03 +0900

    Support lens >= 4.4. `defaultRules` has been removed since lens 4.4.

--- Web/Twitter/Conduit/Types/TH.hs
+++ Web/Twitter/Conduit/Types/TH.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 module Web.Twitter.Conduit.Types.TH
        ( makeLenses
        ) where
@@ -5,5 +7,12 @@ module Web.Twitter.Conduit.Types.TH
 import Control.Lens hiding (makeLenses)
 import Language.Haskell.TH
 
+#if MIN_VERSION_lens(4, 4, 0)
+makeLenses :: Name -> Q [Dec]
+makeLenses = makeLensesWith (lensRules & lensField .~ myFieldToDef)
+  where
+    myFieldToDef _ n = [TopName (mkName (nameBase n))]
+#else
 makeLenses :: Name -> Q [Dec]
 makeLenses = makeLensesWith (defaultRules & lensField .~ Just)
+#endif
