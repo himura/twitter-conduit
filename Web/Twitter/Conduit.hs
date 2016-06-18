082b0c3a8916cbca42d048784ee34cff43946d98
Takahiro HIMURA <taka@himura.jp>
2016-06-18 20:41:29 +0900

    Add DEPRECATED messages
    Re-exporting `Parameters` module will be discontinued in future release.

--- Web/Twitter/Conduit.hs
+++ Web/Twitter/Conduit.hs
@@ -19,7 +19,6 @@ module Web.Twitter.Conduit
        -- * Re-exports
          module Web.Twitter.Conduit.Api
        , module Web.Twitter.Conduit.Cursor
-       , module Web.Twitter.Conduit.Parameters
        , module Web.Twitter.Conduit.Request
        , module Web.Twitter.Conduit.Response
        , module Web.Twitter.Conduit.Status
@@ -38,6 +37,12 @@ module Web.Twitter.Conduit
        , sourceWithSearchResult
        , sourceWithSearchResult'
 
+       -- * 'Web.Twitter.Conduit.Parameters'
+       , Parameters.ListParam(..)
+       , Parameters.MediaData(..)
+       , Parameters.UserListParam(..)
+       , Parameters.UserParam(..)
+
        -- * re-exports
        , OAuth (..)
        , Credential (..)
@@ -45,21 +50,53 @@ module Web.Twitter.Conduit
        , Manager
        , newManager
        , tlsManagerSettings
+
+       -- * deprecated
+       , contributorDetails
+       , count
+       , cursor
+       , displayCoordinates
+       , excludeReplies
+       , filterLevel
+       , follow
+       , inReplyToStatusId
+       , includeEntities
+       , includeMyRetweet
+       , includeRts
+       , includeUserEntities
+       , lang
+       , language
+       , locale
+       , map
+       , maxId
+       , mediaIds
+       , page
+       , possiblySensitive
+       , replies
+       , sinceId
+       , skipStatus
+       , stallWarnings
+       , trimUser
+       , until
        ) where
 
 import Web.Twitter.Conduit.Api
 import Web.Twitter.Conduit.Base
 import Web.Twitter.Conduit.Cursor
-import Web.Twitter.Conduit.Parameters
+import qualified Web.Twitter.Conduit.Parameters as Parameters
 import Web.Twitter.Conduit.Request
 import Web.Twitter.Conduit.Response
 import Web.Twitter.Conduit.Status
 import Web.Twitter.Conduit.Stream
 import Web.Twitter.Conduit.Types
+import Web.Twitter.Types
 
 import Data.Default (def)
+import Data.Time.Calendar (Day)
 import Network.HTTP.Conduit (Manager, newManager, tlsManagerSettings)
 import Web.Authenticate.OAuth
+
+import Prelude hiding (map, until)
 
 -- for haddock
 import Data.Conduit
@@ -185,3 +222,107 @@ import Control.Lens
 --                                   , status ^. statusText
 --                                   ]
 -- @
+
+contributorDetails :: Parameters.HasContributorDetailsParam a => Lens' a (Maybe Bool)
+contributorDetails = Parameters.contributorDetails
+{-# DEPRECATED contributorDetails "Please use Web.Twitter.Conduit.Parameters.contributorDetails" #-}
+
+count :: Parameters.HasCountParam a => Lens' a (Maybe Integer)
+count = Parameters.count
+{-# DEPRECATED count "Please use Web.Twitter.Conduit.Parameters.count" #-}
+
+cursor :: Parameters.HasCursorParam a => Lens' a (Maybe Integer)
+cursor = Parameters.cursor
+{-# DEPRECATED cursor "Please use Web.Twitter.Conduit.Parameters.cursor" #-}
+
+displayCoordinates :: Parameters.HasDisplayCoordinatesParam a => Lens' a (Maybe Bool)
+displayCoordinates = Parameters.displayCoordinates
+{-# DEPRECATED displayCoordinates "Please use Web.Twitter.Conduit.Parameters.displayCoordinates" #-}
+
+excludeReplies :: Parameters.HasExcludeRepliesParam a => Lens' a (Maybe Bool)
+excludeReplies = Parameters.excludeReplies
+{-# DEPRECATED excludeReplies "Please use Web.Twitter.Conduit.Parameters.excludeReplies" #-}
+
+filterLevel :: Parameters.HasFilterLevelParam a => Lens' a (Maybe T.Text)
+filterLevel = Parameters.filterLevel
+{-# DEPRECATED filterLevel "Please use Web.Twitter.Conduit.Parameters.filterLevel" #-}
+
+follow :: Parameters.HasFollowParam a => Lens' a (Maybe Bool)
+follow = Parameters.follow
+{-# DEPRECATED follow "Please use Web.Twitter.Conduit.Parameters.follow" #-}
+
+inReplyToStatusId :: Parameters.HasInReplyToStatusIdParam a => Lens' a (Maybe Integer)
+inReplyToStatusId = Parameters.inReplyToStatusId
+{-# DEPRECATED inReplyToStatusId "Please use Web.Twitter.Conduit.Parameters.inReplyToStatusId" #-}
+
+includeEntities :: Parameters.HasIncludeEntitiesParam a => Lens' a (Maybe Bool)
+includeEntities = Parameters.includeEntities
+{-# DEPRECATED includeEntities "Please use Web.Twitter.Conduit.Parameters.includeEntities" #-}
+
+includeMyRetweet :: Parameters.HasIncludeMyRetweetParam a => Lens' a (Maybe Bool)
+includeMyRetweet = Parameters.includeMyRetweet
+{-# DEPRECATED includeMyRetweet "Please use Web.Twitter.Conduit.Parameters.includeMyRetweet" #-}
+
+includeRts :: Parameters.HasIncludeRtsParam a => Lens' a (Maybe Bool)
+includeRts = Parameters.includeRts
+{-# DEPRECATED includeRts "Please use Web.Twitter.Conduit.Parameters.includeRts" #-}
+
+includeUserEntities :: Parameters.HasIncludeUserEntitiesParam a => Lens' a (Maybe Bool)
+includeUserEntities = Parameters.includeUserEntities
+{-# DEPRECATED includeUserEntities "Please use Web.Twitter.Conduit.Parameters.includeUserEntities" #-}
+
+lang :: Parameters.HasLangParam a => Lens' a (Maybe T.Text)
+lang = Parameters.lang
+{-# DEPRECATED lang "Please use Web.Twitter.Conduit.Parameters.lang" #-}
+
+language :: Parameters.HasLanguageParam a => Lens' a (Maybe T.Text)
+language = Parameters.language
+{-# DEPRECATED language "Please use Web.Twitter.Conduit.Parameters.language" #-}
+
+locale :: Parameters.HasLocaleParam a => Lens' a (Maybe T.Text)
+locale = Parameters.locale
+{-# DEPRECATED locale "Please use Web.Twitter.Conduit.Parameters.locale" #-}
+
+map :: Parameters.HasMapParam a => Lens' a (Maybe Bool)
+map = Parameters.map
+{-# DEPRECATED map "Please use Web.Twitter.Conduit.Parameters.map" #-}
+
+maxId :: Parameters.HasMaxIdParam a => Lens' a (Maybe Integer)
+maxId = Parameters.maxId
+{-# DEPRECATED maxId "Please use Web.Twitter.Conduit.Parameters.maxId" #-}
+
+mediaIds :: Parameters.HasMediaIdsParam a => Lens' a (Maybe [Integer])
+mediaIds = Parameters.mediaIds
+{-# DEPRECATED mediaIds "Please use Web.Twitter.Conduit.Parameters.mediaIds" #-}
+
+page :: Parameters.HasPageParam a => Lens' a (Maybe Integer)
+page = Parameters.page
+{-# DEPRECATED page "Please use Web.Twitter.Conduit.Parameters.page" #-}
+
+possiblySensitive :: Parameters.HasPossiblySensitiveParam a => Lens' a (Maybe Bool)
+possiblySensitive = Parameters.possiblySensitive
+{-# DEPRECATED possiblySensitive "Please use Web.Twitter.Conduit.Parameters.possiblySensitive" #-}
+
+replies :: Parameters.HasRepliesParam a => Lens' a (Maybe T.Text)
+replies = Parameters.replies
+{-# DEPRECATED replies "Please use Web.Twitter.Conduit.Parameters.replies" #-}
+
+sinceId :: Parameters.HasSinceIdParam a => Lens' a (Maybe Integer)
+sinceId = Parameters.sinceId
+{-# DEPRECATED sinceId "Please use Web.Twitter.Conduit.Parameters.sinceId" #-}
+
+skipStatus :: Parameters.HasSkipStatusParam a => Lens' a (Maybe Bool)
+skipStatus = Parameters.skipStatus
+{-# DEPRECATED skipStatus "Please use Web.Twitter.Conduit.Parameters.skipStatus" #-}
+
+stallWarnings :: Parameters.HasStallWarningsParam a => Lens' a (Maybe Bool)
+stallWarnings = Parameters.stallWarnings
+{-# DEPRECATED stallWarnings "Please use Web.Twitter.Conduit.Parameters.stallWarnings" #-}
+
+trimUser :: Parameters.HasTrimUserParam a => Lens' a (Maybe Bool)
+trimUser = Parameters.trimUser
+{-# DEPRECATED trimUser "Please use Web.Twitter.Conduit.Parameters.trimUser" #-}
+
+until :: Parameters.HasUntilParam a => Lens' a (Maybe Day)
+until = Parameters.until
+{-# DEPRECATED until "Please use Web.Twitter.Conduit.Parameters.until" #-}
