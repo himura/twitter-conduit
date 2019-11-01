a4c47cf746f74edbc84bf82f5038fbdab2993b9f
Takahiro HIMURA <taka@himura.jp>
2019-11-02 07:04:28 +0900

    Support per-API label type.
    The type of the #cursor parameter depends on API endpoint

--- Web/Twitter/Conduit/Request/Internal.hs
+++ Web/Twitter/Conduit/Request/Internal.hs
@@ -1,29 +1,45 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Web.Twitter.Conduit.Request.Internal where
 
 import Control.Lens
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as S8
+import Data.Proxy
 import Data.Text (Text)
 import qualified Data.Text.Encoding as T
 import Data.Time.Calendar (Day)
-import GHC.TypeLits (Symbol)
+import GHC.OverloadedLabels
+import GHC.TypeLits
 import GHC.Types (Constraint)
 import qualified Network.HTTP.Types as HT
 
-type family HasParam (key :: Symbol) (ks :: [Symbol]) :: Constraint where
-    HasParam (key :: Symbol) (key ': ks) = ()
-    HasParam (key :: Symbol) (k ': ks) = HasParam key ks
+data Param label t = label := t
+
+type family HasParam (label :: Symbol) (paramType :: t) (params :: [Param Symbol t]) :: Constraint where
+    HasParam label paramType ((label ':= paramType) ': ks) = ()
+    HasParam label paramType ((label' ':= paramType') ': ks) = HasParam label paramType ks
 
 type APIQuery = [APIQueryItem]
 type APIQueryItem = (ByteString, PV)
 
-class Parameters a where
-    params :: Lens' a APIQuery
+class Parameters req where
+    type SupportedParams req :: [Param Symbol t]
+    params :: Lens' req APIQuery
 
 data PV
     = PVInteger { unPVInteger :: Integer }
@@ -46,3 +62,22 @@ paramValueBS (PVIntegerArray iarr) = S8.intercalate "," $ map (S8.pack . show) i
 paramValueBS (PVStringArray iarr) = S8.intercalate "," $ map T.encodeUtf8 iarr
 paramValueBS (PVDay day) = S8.pack . show $ day
 
+wrappedParam :: (Parameters p, KnownSymbol label)
+             => Proxy label
+             -> (a -> PV)
+             -> (PV -> a)
+             -> Lens' p (Maybe a)
+wrappedParam proxy wrap unwrap = lens getter setter
+   where
+     key = S8.pack $ symbolVal proxy
+     getter = preview $ params . to (lookup key) . _Just . to unwrap
+     setter = flip (over params . replace key)
+     replace k (Just v) = ((k, wrap v):) . dropAssoc k
+     replace k Nothing = dropAssoc k
+     dropAssoc k = filter ((/= k) . fst)
+
+class (Parameters req, HasParam label a (SupportedParams req)) => Parameter (label :: Symbol) req a | label -> a where
+    lParam :: Lens' req (Maybe a)
+
+instance (Parameter label req a, Parameters req, HasParam label a (SupportedParams req), Functor f, lens ~ (((Maybe a) -> f (Maybe a)) -> req -> f req)) => IsLabel label lens where
+    fromLabel = lParam @ label
