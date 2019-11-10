3ee4a66c3c9baefc0f68af1b58656555ff1dfa42
Takahiro HIMURA <taka@himura.jp>
2019-11-10 23:08:53 +0900

    Do not specify proxy in test codes.

--- tests/TestUtils.hs
+++ tests/TestUtils.hs
@@ -2,16 +2,9 @@
 
 module TestUtils (getTWInfo) where
 
-import Web.Twitter.Conduit
-
-import qualified Network.URI as URI
-import Network.HTTP.Conduit
-import qualified Data.Map as M
 import qualified Data.ByteString.Char8 as S8
-import qualified Data.CaseInsensitive as CI
-import Control.Applicative
 import System.Environment
-import Control.Lens
+import Web.Twitter.Conduit
 
 getOAuthTokens :: IO (OAuth, Credential)
 getOAuthTokens = do
@@ -31,21 +24,7 @@ getOAuthTokens = do
   where
     getEnv' = (S8.pack <$>) . getEnv
 
-getProxyEnv :: IO (Maybe Proxy)
-getProxyEnv = do
-    env <- M.fromList . over (mapped . _1) CI.mk <$> getEnvironment
-    let u = M.lookup "https_proxy" env <|>
-            M.lookup "http_proxy" env <|>
-            M.lookup "proxy" env >>= URI.parseURI >>= URI.uriAuthority
-    return $ Proxy <$> (S8.pack . URI.uriRegName <$> u) <*> (parsePort . URI.uriPort <$> u)
-  where
-    parsePort :: String -> Int
-    parsePort []       = 8080
-    parsePort (':':xs) = read xs
-    parsePort xs       = error $ "port number parse failed " ++ xs
-
 getTWInfo :: IO TWInfo
 getTWInfo = do
-    pr <- getProxyEnv
     (oa, cred) <- getOAuthTokens
-    return $ (setCredential oa cred def) { twProxy = pr }
+    return $ setCredential oa cred def
