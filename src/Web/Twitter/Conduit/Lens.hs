22c439ca2c5c1076878a605ea7b509cb204f6a5a
Takahiro HIMURA <taka@himura.jp>
2021-11-22 22:51:17 +0900

    .

--- src/Web/Twitter/Conduit/Lens.hs
+++ src/Web/Twitter/Conduit/Lens.hs
@@ -2,13 +2,13 @@
 
 module Web.Twitter.Conduit.Lens (
     -- * 'TT.Response'
-    TT.Response,
+    TT.APIResponse,
     responseStatus,
     responseBody,
     responseHeaders,
 
     -- * 'TT.TwitterErrorMessage'
-    TT.TwitterErrorMessage,
+    TT.TwitterError,
     twitterErrorMessage,
     twitterErrorCode,
 
@@ -17,32 +17,29 @@ module Web.Twitter.Conduit.Lens (
     previousCursor,
     nextCursor,
     contents,
-
-    -- * Re-exports
-    TT.TwitterError (..),
 ) where
 
-import Control.Lens
+import Control.Lens (Lens, Lens')
 import Data.Text (Text)
 import Network.HTTP.Types (ResponseHeaders, Status)
 import qualified Web.Twitter.Conduit.Cursor as TT
 import qualified Web.Twitter.Conduit.Response as TT
 
 -- * Lenses for 'TT.Response'
-responseStatus :: forall responseType. Lens' (TT.Response responseType) Status
-responseStatus afb s = (\b -> s {TT.responseStatus = b}) <$> afb (TT.responseStatus s)
+responseStatus :: forall responseType. Lens' (TT.APIResponse responseType) Status
+responseStatus afb s = (\b -> s {TT.apiResponseStatus = b}) <$> afb (TT.apiResponseStatus s)
 
-responseHeaders :: forall responseType. Lens' (TT.Response responseType) ResponseHeaders
-responseHeaders afb s = (\b -> s {TT.responseHeaders = b}) <$> afb (TT.responseHeaders s)
+responseHeaders :: forall responseType. Lens' (TT.APIResponse responseType) ResponseHeaders
+responseHeaders afb s = (\b -> s {TT.apiResponseHeaders = b}) <$> afb (TT.apiResponseHeaders s)
 
-responseBody :: forall a b. Lens (TT.Response a) (TT.Response b) a b
-responseBody afb s = (\b -> s {TT.responseBody = b}) <$> afb (TT.responseBody s)
+responseBody :: forall a b. Lens (TT.APIResponse a) (TT.APIResponse b) a b
+responseBody afb s = (\b -> s {TT.apiResponseBody = b}) <$> afb (TT.apiResponseBody s)
 -- * Lenses for 'TT.TwitterErrorMessage'
 
-twitterErrorCode :: Lens' TT.TwitterErrorMessage Int
+twitterErrorCode :: Lens' TT.TwitterError Int
 twitterErrorCode afb s = (\b -> s {TT.twitterErrorCode = b}) <$> afb (TT.twitterErrorCode s)
 
-twitterErrorMessage :: Lens' TT.TwitterErrorMessage Text
+twitterErrorMessage :: Lens' TT.TwitterError Text
 twitterErrorMessage afb s = (\b -> s {TT.twitterErrorMessage = b}) <$> afb (TT.twitterErrorMessage s)
 -- * Lenses for 'TT.WithCursor'
 previousCursor :: forall cursorType cursorKey wrapped. Lens' (TT.WithCursor cursorType cursorKey wrapped) (Maybe cursorType)
--- src/Web/Twitter/Conduit/Response.hs
+++ src/Web/Twitter/Conduit/Response.hs
@@ -1,51 +1,153 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Web.Twitter.Conduit.Response (
-    Response (..),
-    TwitterError (..),
-    TwitterErrorMessage (..),
-) where
+module Web.Twitter.Conduit.Response where
 
-import Control.Exception
+import Control.Monad ( (<=<) )
+import Control.Exception ( Exception, throwIO )
 import Data.Aeson
-import Data.Data
+    ( FromJSON(parseJSON),
+      Value,
+      eitherDecode',
+      json,
+      fromJSON,
+      Result(Error, Success),
+      ToJSON(toEncoding) )
+import qualified Data.ByteString as S
+import Data.Data (Data, Typeable)
 import qualified Data.Text as T
 import Network.HTTP.Types (ResponseHeaders, Status)
+import qualified Network.HTTP.Types as HTTPTypes
+import qualified Network.HTTP.Client as HTTP
+import Web.Twitter.Conduit.Internal.AesonUtil
+    ( stripPrefixGenericParseJSON
+    , stripPrefixGenericToEncoding
+    )
+import GHC.Generics (Generic)
+import qualified Data.ByteString.Lazy as L
+import Data.Attoparsec.ByteString
+    ( endOfInput, eitherResult, maybeResult, parseWith, parse )
+import Data.Attoparsec.ByteString.Char8 (decimal, signed)
 
-data Response responseType = Response
-    { responseStatus :: Status
-    , responseHeaders :: ResponseHeaders
-    , responseBody :: responseType
+data APIResponse responseType = APIResponse
+    { apiResponseStatus :: Status
+    , apiResponseHeaders :: ResponseHeaders
+    , apiResponseRateLimitStatus :: RateLimitStatus
+    , apiResponseBody :: responseType
     }
-    deriving (Show, Eq, Typeable, Functor, Foldable, Traversable)
+    deriving (Show, Eq, Functor, Foldable, Traversable)
 
-data TwitterError
-    = FromJSONError String
-    | TwitterErrorResponse Status ResponseHeaders [TwitterErrorMessage]
-    | TwitterUnknownErrorResponse Status ResponseHeaders Value
-    | TwitterStatusError Status ResponseHeaders Value
-    deriving (Show, Typeable, Eq)
+data RateLimitStatus =
+    RateLimitStatus
+        { rateLimitStatusLimit :: Maybe Int -- ^ the rate limit ceiling for that given endpoint (HTTP header: x-rate-limit-limit)
+        , rateLimitStatusRemaining :: Maybe Int -- ^ the number of requests left for the 15-minute window (HTTP header: x-rate-limit-remaining)
+        , rateLimitStatusReset :: Maybe Int -- ^ the remaining window before the rate limit resets, in UTC epoch seconds (HTTP header: x-rate-limit-reset)
+        }
+    deriving (Show, Eq, Ord, Generic)
 
-instance Exception TwitterError
+data APIException =
+    APIException
+        { apiExceptionStatus :: Status
+        , apiExceptionHeaders :: ResponseHeaders
+        , apiExceptionContext :: APIExceptionContent
+        }
+    deriving (Show, Eq, Generic, Typeable)
+instance Exception APIException
+
+data APIExceptionContent
+    = RateLimitExceeded RateLimitStatus TwitterErrors
+    | ErrorResponse TwitterErrors
+    | UnknownErrorResponse L.ByteString
+    | ParseJSONError String
+    | FromJSONError String Value
+    deriving (Show, Eq, Generic)
+
+newtype TwitterErrors =
+    TwitterErrors
+        { twitterErrors :: [TwitterError]
+        }
+    deriving (Show, Eq, Ord, Generic)
+
+instance FromJSON TwitterErrors where
+    parseJSON = stripPrefixGenericParseJSON "twitter"
+
+instance ToJSON TwitterErrors where
+    toEncoding = stripPrefixGenericToEncoding "twitter"
 
 -- | Twitter Error Messages
 --
--- see detail: <https://dev.twitter.com/docs/error-codes-responses>
-data TwitterErrorMessage = TwitterErrorMessage
+-- see detail: <https://developer.twitter.com/en/docs/twitter-ads-api/response-codes>
+data TwitterError = TwitterError
     { twitterErrorCode :: Int
     , twitterErrorMessage :: T.Text
     }
-    deriving (Show, Eq, Ord, Data, Typeable)
+    deriving (Show, Eq, Ord, Generic, Data, Typeable)
 
-instance Enum TwitterErrorMessage where
+instance Enum TwitterError where
     fromEnum = twitterErrorCode
-    toEnum a = TwitterErrorMessage a T.empty
+    toEnum a = TwitterError a T.empty
 
-instance FromJSON TwitterErrorMessage where
-    parseJSON (Object o) =
-        TwitterErrorMessage
-            <$> o .: "code"
-            <*> o .: "message"
-    parseJSON v = fail $ "unexpected: " ++ show v
+instance FromJSON TwitterError where
+    parseJSON = stripPrefixGenericParseJSON "twitterError"
+instance ToJSON TwitterError where
+    toEncoding = stripPrefixGenericToEncoding "twitterError"
+
+getRateLimitStatus :: HTTP.Response a -> RateLimitStatus
+getRateLimitStatus res =
+    RateLimitStatus
+        { rateLimitStatusLimit = getHeaderInt "x-rate-limit-limit"
+        , rateLimitStatusRemaining = getHeaderInt "x-rate-limit-remaining"
+        , rateLimitStatusReset = getHeaderInt "x-rate-limit-reset"
+        }
+  where
+    getHeaderInt name = lookup name (HTTP.responseHeaders res) >>= parseInt
+    parseInt = maybeResult . parse (signed decimal <* endOfInput)
+
+eitherDecodeBodyReader :: HTTP.BodyReader -> IO (Either APIExceptionContent Value)
+eitherDecodeBodyReader br =
+    eitherFormatError <$> parseWith br json S.empty
+  where
+    eitherFormatError = either (Left . ParseJSONError) Right . eitherResult
+
+eitherFromJSON :: FromJSON a => Value -> Either APIExceptionContent a
+eitherFromJSON value =
+    case fromJSON value of
+        Success body -> Right body
+        Error err -> Left $ FromJSONError err value
+
+handleResponse ::
+       FromJSON responseType
+    => HTTP.Response HTTP.BodyReader
+    -> IO (Either APIException (APIResponse responseType))
+handleResponse res =
+    case HTTP.responseStatus res of
+        st
+            | st >= HTTPTypes.status200 && st < HTTPTypes.status300 -> do
+                deserResult <- eitherDecodeBodyReader $ HTTP.responseBody res
+                case deserResult >>= eitherFromJSON of
+                    Left err ->
+                        return . Left $
+                        APIException st (HTTP.responseHeaders res) err
+                    Right value ->
+                        return . Right $
+                        APIResponse
+                            { apiResponseStatus = st
+                            , apiResponseHeaders = HTTP.responseHeaders res
+                            , apiResponseRateLimitStatus = getRateLimitStatus res
+                            , apiResponseBody = value
+                            }
+            | otherwise -> do
+                lbs <- HTTP.brReadSome (HTTP.responseBody res) 4096
+                let err =
+                        either (const $ UnknownErrorResponse lbs) ErrorResponse $
+                        eitherDecode' lbs
+                return . Left $
+                    APIException st (HTTP.responseHeaders res) err
+
+handleResponseThrow ::
+       FromJSON responseType
+    => HTTP.Response HTTP.BodyReader
+    -> IO (APIResponse responseType)
+handleResponseThrow = either throwIO return <=< handleResponse
